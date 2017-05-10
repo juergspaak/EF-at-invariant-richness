5c5a62ecb56d3e3422c1798bf9cd5e93a84d78ab
Jurg <Jurg>
2017-05-10 18:57:31 +0200

    Save values in dictionaries
    Values for repl community are stored in dictionaries
    Renaming c->b, r->u, s->c. To be conform with paper

--- community_construction.py
+++ community_construction.py
@@ -10,6 +10,7 @@ and calculate its deltaEF
 import numpy as np
 from numpy.random import uniform as uni
 from scipy.integrate import quad
+import community_construction as test
 
 
 sqrt = np.sqrt(3) #is needed often in the program
@@ -18,19 +19,21 @@ n = 20
 def rand_par_repl(count=False,p = 'rand', ave_max = 0.5, e_max = 1,
                   ave_min = -0.5, e_min = -1):
     """ returns randomized parameters for one ecosystem
-    av means average, t_ means stdv/average,
+    av means average, t means stdv/average,
     t values are always between -1/sqrt(3), 1/sqrt(3)
-    to understand the equations consult supplement data, appendix G, folder results"""
+    to understand the equations consult supplement data 7"""
     
     # generate distribution of per capita contributions for species types
-    t_fc, t_fr, t_fs = uni(-1/sqrt, 1/sqrt,3) # stdv/mean
-    avfc, avfr, avfs = uni(0.5,1.5,3) #averages of f
+    t_fb, t_fu, t_fc = uni(-1/sqrt, 1/sqrt,3) # stdv/mean
+    avfb, avfu, avfc = uni(0.5,1.5,3) #averages of f
+    f = {'avb':avfb,'avu':avfu,'avc':avfc,\
+         'tb':t_fb,'tu':t_fu,'tc':t_fc}
     
-
+    #avoids infinite loops
     counter = 0
     # fixed parameters, will only be changed if "no" community can be found
     # having these parameters
-    avec = uni(ave_min,ave_max) # average effect on C species
+    e = {'avb':uni(ave_min,ave_max)} # average effect on C species
     alpha = uni(-0.95,-0.05) # interaction coecfficient
     comp = -alpha*n/(1-alpha*(n-1)) #effective competition , computed
     
@@ -41,57 +44,62 @@ def rand_par_repl(count=False,p = 'rand', ave_max = 0.5, e_max = 1,
     
     #randomly generate communities, until one fullfills coexistence conditions
     #attention, changing settings might turn this into an infinite loop
-    while not (counter and coex_test_repl(*parameters)):
-        # chosen such that avmur,acmuc/(p*avmuc+q*avmuc)>comp
-        avmuc = uni(0,10)
-        avmur = uni(avmuc*comp*p/(1-q*comp),
-                             min(avmuc*(1-p*comp)/(q*comp),10))
+    while not (counter and coex_test_repl2(mu, e,f,comp,alpha,p)):
+    # chosen such that min(mu['avb'],mu['avu'])/(p*mu['avb']+q*mu['avu'])>comp
+        mu = {'avb': uni(0,10)}
+        mu['avu'] = uni(mu['avb']*comp*p/(1-q*comp),
+                             min(mu['avb']*(1-p*comp)/(q*comp),10))
         
         
-        treshhold1 = comp*(p*avmuc+q*avmur)
-        # chosen such that min (mur,muc) > treshhold1, i.e. coexist
-        t_muc = uni(-(1-treshhold1/avmuc)/sqrt,(1-treshhold1/avmuc)/sqrt)
-        t_mur = uni(-(1-treshhold1/avmur)/sqrt,(1-treshhold1/avmur)/sqrt)
+        tresh1 = comp*(p*mu['avb']+q*mu['avu'])
+        # chosen such that min (mu_u,mu_b) > tresh1, i.e. coexist
+        mu['tb'] = uni(-(1-tresh1/mu['avb'])/sqrt,(1-tresh1/mu['avb'])/sqrt)
+        mu['tu'] = uni(-(1-tresh1/mu['avu'])/sqrt,(1-tresh1/mu['avu'])/sqrt)
         
-        # acmus*(1-ave_min)/(p*avmuc)(1-avec)+q*avmus(1-ave_min))>comp
-        treshhold2 = avmuc*(1-avec)*p*comp/(1-comp*q)/(1-ave_min)
+        
+        # mu['avc']*(1-ave_min) must be able to coexist in changed site
+        tresh2 = mu['avb']*(1-e['avb'])*p*comp/(1-comp*q)/(1-ave_min)
         # we always have treshhold2<treshhold1
-        avmus = uni(treshhold2,treshhold1)
+        mu['avc'] = uni(tresh2,tresh1)
         
-        # ensure, that min(mus) fullfills above conditions
-        dist = min(treshhold1/avmus-1, 1-treshhold2/avmus)/sqrt
-        t_mus = uni(-dist,dist)
+        # ensure, that min(mu_c) fullfills above conditions
+        dist = min(tresh1/mu['avc']-1, 1-tresh2/mu['avc'])/sqrt
+        mu['tc'] = uni(-dist,dist)
         
-        # choose min, s.t. avmus*(1-aves) fullfills coexistence conditions
-        treshhold1 = max(1-avmuc/avmus*(1-avec)*(1-comp*p)/(q*comp),ave_min)
-        # choose max, s.t. avmuc*(1-avec) fullfills coexistence conditions
-        treshhold2 = min(1-avmuc/avmus*(1-avec)/(1-comp*q)*(p*comp),ave_max)
-        aves = uni(treshhold1, treshhold2)
+        # choose min, s.t. mu['avc']*(1-e['avc']) fullfills coexistence conditions
+        tresh1 = max(1-mu['avb']/mu['avc']*(1-e['avb'])*(1-comp*p)\
+                     /(q*comp),ave_min)
+        # choose max, s.t. mu['avc']*(1-e['avc']) fullfills coexistence conditions
+        tresh2 = min(1-mu['avb']/mu['avc']*(1-e['avb'])/(1-comp*q)\
+                     *(p*comp),ave_max)
+        e['avc'] = uni(tresh1, tresh2)
         
         # choose borders, that e_i are within [e_min, e_max]
-        minimum = min(np.abs([e_max/avec-1, 1-e_min/avec]))
-        t_ec = uni(-minimum/sqrt,minimum/sqrt)
-        minimum = min(np.abs([e_max/aves-1, 1-e_min/aves]))
-        t_es = uni(-minimum/sqrt,minimum/sqrt)
+        minimum = min(np.abs([e_max/e['avc']-1, 1-e_min/e['avb']]))
+        e['tb'] = uni(-minimum/sqrt,minimum/sqrt)
+        minimum = min(np.abs([e_max/e['avc']-1, 1-e_min/e['avc']]))
+        e['tc'] = uni(-minimum/sqrt,minimum/sqrt)
+        
+        # average growthsrates in changed site of the species types
+        mu['avb_change'] = mu['avb']*e['avb']*(1/e['avb']-1 - mu['tb']*e['tb'])
+        mu['avc_change'] = mu['avc']*e['avc']*(1/e['avc']-1 - mu['tc']*e['tc'])
+        # average growthrate of entire community
+        mu['av_change'] = p*mu['avb_change']+q*mu['avc_change']
         
         # reference types are assumed to have e_i = 1, always         
-        #aver = 1 #change if desired differently
-        #t_er = 0
+        # e['avu'] = 1 #change if desired differently
+        # e['tu'] = 0
         if counter >10000: # avoids infinite loops, happens about 1/150
             counter = 0 
-            avec = uni(0,0.5) # redefine new sensitivity
+            e['avb'] = uni(0,0.5) # redefine new sensitivity
             alpha = uni(-0.95,-0.05) # interaction coecfficient
             comp =  -alpha*n/(1-alpha*(n-1))
-        
-        parameters = avmuc,t_muc, avmur, t_mur, avmus, t_mus, avec, \
-              t_ec, aves,t_es,comp,t_fc,t_fr,t_fs, \
-              avfc,avfr,avfs,alpha,p
         counter+=1
-    return  parameters # exited while loop, community fullfills coexistence
-    
-def coex_test_repl(avmuc,t_muc, avmur, t_mur, avmus, t_mus, avec, \
-              t_ec, aves,t_es,comp,t_fc,t_fr,t_fs, \
-              avfc,avfr,avfs,alpha,p):
+
+    return  mu, e,f,comp,alpha,p # exited while loop, community fullfills coexistence
+     
+  
+def coex_test_repl(mu, e,f,comp,alpha,p):
     """tests if coexistence is given in changed site
     
     input should be the output of rand_par_repl
@@ -100,90 +108,75 @@ def coex_test_repl(avmuc,t_muc, avmur, t_mur, avmus, t_mus, avec, \
     nor that U species cannot survive in changed site and vice versa
     These conditions are always fullfilled by the chosen parameter settings
     By changing the above parameters this might become necessary"""
-    
-    
-    q = 1-p
-    sign_c, sign_s = np.sign([avec, aves]) #changes sign of inequalities
-    
-    # coex in stressed site
-    avmuc_s = avmuc*avec*(1/avec-1 - t_muc*t_ec) #average growth rates of
-    avmus_s = avmus*aves*(1/aves-1 - t_mus*t_es) #species in changed site
+
     #computes the growthrate of one species
-    mu_str = lambda avmu, tmu, ave, te, u_i: \
-                    avmu*(1+u_i*tmu*sqrt)*ave*(1/ave-(1+u_i*te*sqrt))
-    # compute growthrates of extrema on boundaries:
-    minimalc1 = mu_str(avmuc, t_muc, avec, t_ec,1) 
-    minimalc2 = mu_str(avmuc, t_muc, avec, t_ec,-1)    
-    minimals1 = mu_str(avmus, t_mus, aves, t_es,1)
-    minimals2 = mu_str(avmus, t_mus, aves, t_es,-1)
-    #minimal growthrate of all species in changed site
-    minimal = min(minimalc1,minimalc2, minimals1, minimals2)\
-                    /(p*avmuc_s+q*avmus_s)
-                    
-    """The following checks whether r species are extinct
-    maximalr1 = mu_str(avmucr t_mur, aver, t_er,1) #maxima on the boundaries
-    maximalr2 = mu_str(avmucr t_mur, aver, t_er,-1)
+    mu_str = lambda x,t: mu['av'+t]*(1+x*mu['t'+t]*sqrt)\
+                *e['av'+t]*(1/e['av'+t]-(1+x*e['t'+t]*sqrt))/mu['av_change']
+
+    # minimal growthrate of all species in changed site, extremum is on boundary
+    minimal = 1
+    for x,t in [[1,'b'],[-1,'b'],[1,'c'],[-1,'c']]:
+        minimal = min(mu_str(x,t),minimal)
+                
+    """The following checks whether u species are extinct
+    maximal = max(mu_str(1,'u'),mu_str(-1,'u')) #maxima on the boundaries
     # maxima in interior
-    loc_r = 0.5*(1/t_er*(1/aver-1)-1/t_mur)
+    loc_r = 0.5*(1/e['tu']*(1/e['avu']-1)-1/mu['tu'])
     if -1 < loc_r < 1:
-        maximalr1 = max(maximalr1, 
-            avmur*(1+loc_r*sqrt*t_mur)*aver*(1/aver-(1+loc_r*sqrt*t_er)))
-    if max(maximalr1, maximalr2)/(p*avmuc_s+q*avmus_s)>comp
+        maximal = max(maximal, mu_str(loc,'u')
+    if maximal/mu['av_change']>comp
         return False"""
-    
+
     if minimal<comp:
         return False
     else:
         return True
-
     
-def EF(avmu1,avmu2,f1,f2,cov1,cov2,alpha,p, adjust):
+def EF_(mu,f,alpha,p,s,cov,adjust=1):
     """ computes the EF of the given system
     
     For computational background see Eq. 6"""
     q = 1-p
     comp = -alpha*n/(1-alpha*(n-1))
-    EF1 = n*f1*avmu1/(1+alpha)*(cov1+1-comp)
-    EF2 = n*f2*avmu2/(1+alpha)*(cov2+1-comp)
-    return p*EF1+q*EF2+adjust*p*q*n*comp/(1+alpha)*(f1-f2)*(avmu1-avmu2)
+    EF1 = n*f['avb']*mu['avb'+s[1]]/(1+alpha)*(cov['b'+s[1]]+1-comp)
+    EF2 = n*f['av'+s[0]]*mu['av'+s[0]+s[1]]/(1+alpha)*(cov[s[0]+s[1]]+1-comp)
+    return p*EF1+q*EF2+adjust*p*q*n*comp/(1+alpha)*(f['avb']-f['av'+s[0]])\
+                            *(mu['avb'+s[1]]-mu['av'+s[0]+s[1]])
     
-def rel_delta_EF_repl(avmuc,t_muc, avmur, t_mur, avmus, t_mus, avec, \
-              t_ec, aves,t_es,comp,t_fc,t_fr,t_fs, \
-              avfc,avfr,avfs,alpha,p, adjust = 1):
+def rel_delta_EF_repl(mu, e,f,comp,alpha,p, adjust = 1):
     """computes \DeltaEF/EF in the case of changing composition
     
     For computational background see Eq. 7
     
     adjust can be set to 0 to see the effect of the adjustmentterms"""
-    covc = t_muc*t_fc #covariances of the relative distributions
-    covr = t_mur*t_fr
-    EF_r = EF(avmuc,avmur,avfc,avfr,covc,covr,alpha,p, adjust)
-    
-    avmuc_s = avmuc*avec*(1/avec-1 - t_muc*t_ec) #average growth rates in
-    avmus_s = avmus*aves*(1/aves-1 - t_mus*t_es) #changed site
-    covc_s = t_fc*(t_muc*(1/avec-1)-t_ec)/(1/avec-1-t_ec*t_muc)
-    covs_s = t_fs*(t_mus*(1/aves-1)-t_es)/(1/aves-1-t_es*t_mus)
-    EF_s = EF(avmuc_s,avmus_s,avfc,avfs,covc_s,covs_s,alpha,p, adjust)
+    cov = {'b': mu['tb']*f['tb'], 'u': mu['tu']*f['tu']} #covariances of the relative distributions
+    cov['b_change'] = f['tb']*(mu['tb']*(1/e['avb']-1)-e['tb'])\
+                        /(1/e['avb']-1-e['tb']*mu['tb'])
+    cov['c_change'] = f['tc']*(mu['tc']*(1/e['avc']-1)-e['tc'])\
+                        /(1/e['avc']-1-e['tc']*mu['tc'])
 
+    EF_r = EF_2(mu,f,alpha,p,['u',''],cov,adjust)
+    EF_s = EF_2(mu,f,alpha,p,['c','_change'],cov,adjust)
     return 100*(EF_s-EF_r)/EF_r #multiply by 100, result in percent
 
 def rand_par_coex(ave_max = 0.5, e_max = 1,
                   ave_min = -0.5,e_min = -1):
     """ returns randomized parameters for one community"""
     
-    ave = uni(ave_min,ave_max) # average sensitivity, in [0.01, 0.99]
+    ave = uni(ave_min,ave_max) # average sensitivity
     alpha = uni(-0.95,-0.05) #interaction coefficient
     parameters = [1,0,0,0,0,0,0] #values that do not coexist
     # runs until community that coexists is found
-    #attention, changing settings might turn this into an infinite loop
+    # WARNING: changing settings might turn this into an infinite loop
     while not (coex_test_coex(*parameters)):
         """to understand why these parameters are chosen in this way, 
         see appendix G in supplement data, folder results"""
         t_f = uni(-1/sqrt, 1/sqrt) # stdv/mean of per capita contribution, in [-1/sqrt(3),1/sqrt(3)]
-        n = np.random.randint(5,21)
-        comp = -alpha*n/(1-alpha*(n-1)) #effective competition , computed
+        n = np.random.randint(5,21) # number of species
+        comp = -alpha*n/(1-alpha*(n-1)) #effective competition
         t_mu = uni(comp-1,1-comp)/sqrt
         minimum = min(np.abs([e_max/ave-1, 1-e_min/ave]))
+        #ensures, that sensitivity e_i  is in [e_min, e_max]
         t_e = uni(-minimum/sqrt,minimum/sqrt)
         parameters = ave,t_e,t_mu,t_f,comp,alpha,n
 
@@ -242,40 +235,33 @@ def asymptotic_function_coex(max_ave_gam=1, test = False):
     
     return 100*(EF_stress-EF)/EF,delta_EF_lin,paras
 
-def asymptotic_function_repl(paras, max_ave_gam=1, test = False):
+def asymptotic_function_repl(mu, e,f,comp,alpha,p
+                              , max_ave_gam=1, test = False):
     if test:
-        ave_gam = {'c':1000,'r':1000,'s':1000}
-        t_gam = {'c':0,'r':0,'s':0}
+        gam = {'avb':1000,'avu':1000,'avc':1000,\
+               'tb':0,'tu':0,'tc':0}
     else:
         temp = uni(0,max_ave_gam,3)
-        ave_gam = {'c':temp[0],'r':temp[1],'s':temp[2]}
+        gam = {'avb':temp[0],'avu':temp[1],'avc':temp[2]}
         temp = uni(-1/sqrt, 1/sqrt,3)
-        t_gam = {'c':temp[0],'r':temp[1],'s':temp[2]}
-    #paras = rand_par_repl(ave_max = 0, e_max = 0)   
-    avmuc,t_muc, avmur, t_mur, avmus, t_mus, avec, \
-              t_ec, aves,t_es,comp,t_fc,t_fr,t_fs, \
-              avfc,avfr,avfs,alpha,p = paras
-    avmu = {'c':avmuc,'r':avmur,'s':avmus}
-    ave = {'c':avec,'r':1,'s':aves}
-    avf = {'c':avfc,'r':avfr,'s':avfs}
-    t_mu = {'c':t_muc,'r':t_mur,'s':t_mus}
-    t_e = {'c':t_ec,'r':0,'s':t_es}
-    t_f = {'c':t_fc,'r':t_fr,'s':t_fs}
-    eco_fun = lambda x,t, N, H: avf[t]*(1+t_f[t]*x*sqrt)*H(x,t)*N(x,t)\
+        gam.update({'tb':temp[0],'tu':temp[1],'tc':temp[2]})
+
+    eco_fun = lambda x,t, N, H: f['av'+t]*(1+f['t'+t]*x*sqrt)*H(x,t)*N(x,t)\
                                    /(N(x,t)+H(x,t))/2
     N = lambda x,t,mu,avmu: (mu(x,t)-comp*avmu)/(1+alpha)
-    mu = lambda x,t: avmu[t]*(1+x*sqrt*t_mu[t])
-    mu_change = lambda x,t: avmu[t]*(1+x*sqrt*t_mu[t])*\
-                            (1-ave[t]*(1+t_e[t]*sqrt*x))
-    N_ref = lambda x,t: N(x,t,mu,p*avmu['c']+(1-p)*avmu['r'])
-    avmu_change = p*avmu['c']*(1-ave['c']*(1+t_mu['c']*t_e['c']))+\
-                    (1-p)*avmu['s']*(1-ave['s']*(1+t_mu['s']*t_e['s']))
-    N_change = lambda x,t: N(x,t,mu_change,avmu_change)
-    H = lambda x,t: ave_gam[t]*(1+t_gam[t]*sqrt*x)*avmu[t]*(1+t_mu[t]*x*sqrt)
-    EF = n*(p*quad(lambda x: eco_fun(x,'c',N_ref,H),-1,1)[0]\
-            +(1-p)*quad(lambda x: eco_fun(x,'r',N_ref,H),-1,1)[0])
-    EF_change = n*(p*quad(lambda x: eco_fun(x,'c',N_change,H),-1,1)[0]\
-            +(1-p)*quad(lambda x: eco_fun(x,'s',N_change,H),-1,1)[0])
-    delta_EF_lin = rel_delta_EF_repl(*paras)
+    mu_ref = lambda x,t: mu['av'+t]*(1+x*sqrt*mu['t'+t])
+    mu_change = lambda x,t: mu['av'+t]*(1+x*sqrt*mu['t'+t])*\
+                            (1-e['av'+t]*(1+e['t'+t]*sqrt*x))
+    N_ref = lambda x,t: N(x,t,mu_ref,p*mu['avb']+(1-p)*mu['avu'])
+    N_change = lambda x,t: N(x,t,mu_change,mu['av_change'])
+    avmu_change = p*mu['avb']*(1-e['avb']*(1+mu['tb']*e['tc']))+\
+                    (1-p)*mu['avc']*(1-e['avc']*(1+mu['tc']*e['tc']))
+    H = lambda x,t: gam['av'+t]*(1+gam['t'+t]*sqrt*x)\
+                    *mu['av'+t]*(1+mu['t'+t]*x*sqrt)
+    EF = n*(p*quad(lambda x: eco_fun(x,'b',N_ref,H),-1,1)[0]\
+            +(1-p)*quad(lambda x: eco_fun(x,'u',N_ref,H),-1,1)[0])
+    EF_change = n*(p*quad(lambda x: eco_fun(x,'b',N_change,H),-1,1)[0]\
+            +(1-p)*quad(lambda x: eco_fun(x,'c',N_change,H),-1,1)[0])
+    delta_EF_lin = rel_delta_EF_repl2(mu, e,f,comp,alpha,p)
     
-    return 100*(EF_change-EF)/EF,delta_EF_lin,paras    
+    return 100*(EF_change-EF)/EF,delta_EF_lin
