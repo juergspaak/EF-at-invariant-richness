6ffe5bbe1aee35d8ba28b56d3c7cfca3278dc310
Juerg Spaak <juerg.spaak@gmail.com>
2017-07-05 11:15:11 +0200

    remove 'rand', add '0.25' and '0.75' for p

--- parameters_construction.py
+++ parameters_construction.py
@@ -9,14 +9,15 @@ from warnings import warn
 warn_message = "The file 'repl, com_para.p' has not been found. It will be "+\
     "generated automatically and saved into the current folder. "+\
     "This file will contain communitiy parameters to be used in the future. "+\
-    "This will take some minutes."
+    "This will take some minutes."   
+    
 def para_return(fun):
     warn(warn_message)
     
     
     num = int(1e5)
-    #generate communities for different p
-    keys = ['0.50','0.95' , 'rand', '0.05']
+    #generate communities for different, 
+    keys = ['0.50','0.05','0.25', '0.75','0.95' ]
     count = 0
     para = {}
     start = timer()
@@ -28,16 +29,16 @@ def para_return(fun):
         para['e>0,'+key] = fun(p=p,ave_min=0,e_min=0, num = num)
         now = timer()-start
         count+=1
-        print("About {}/8 have been computed so far \n".format(count)+
+        print("About {}/10 have been computed so far \n".format(count)+
                "time needed so far:{} seconds\nFinish ".format(now)+
-               "within approximately {} seconds\n".format(now/count*(8-count)))
+               "within approximately {} seconds\n".format(now/count*(10-count)))
         
         para['e<0,'+key] = fun(p=p,ave_max=0,e_max=0,num = num)
         now = timer()-start
         count+=1
-        print("About {}/8 have been computed so far \n".format(count)+
+        print("About {}/10 have been computed so far \n".format(count)+
                "time needed so far:{} seconds\nFinish ".format(now)+
-               "within approximately {} seconds\n".format(now/count*(8-count)))
+               "within approximately {} seconds\n".format(now/count*(10-count)))
     
     #save data for future use  
     pickle.dump(para, open("repl, com_para.p","wb"))
